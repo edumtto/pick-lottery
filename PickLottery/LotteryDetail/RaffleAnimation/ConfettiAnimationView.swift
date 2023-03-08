829bf0f96c414fdc025e6f8a1f5404017af41c7a
Eduardo Oliveira <edumottaoliveira@gmail.com>
2023-03-07 20:56:04 -0800

    Improve confetti animation

--- PickLottery/LotteryDetail/RaffleAnimation/ConfettiAnimationView.swift
+++ PickLottery/LotteryDetail/RaffleAnimation/ConfettiAnimationView.swift
@@ -15,7 +15,10 @@ struct ConfettiAnimationView_Previews: PreviewProvider {
 }
 
 struct ConfettiView: View {
-    @State private var confetti: [Confetti] = []
+    @State private var confetti: [Confetti] = .init(repeating: Confetti(), count: 10)
+    @State private var timer: Timer?
+    @State var counter: Int = 0
+    let maxConfetti: Int = 50
     
     var body: some View {
         ZStack {
@@ -25,31 +28,39 @@ struct ConfettiView: View {
                         .fill(confetti.color)
                         .frame(width: confetti.size, height: 2 * confetti.size)
                         //.rotationEffect(.degrees(confetti.rotationAngle))
-                        .rotationEffect(.degrees(confetti.rotationBase))
-                        .rotation3DEffect(.degrees(confetti.rotationAnimation), axis: confetti.axis, perspective: 0.5)
-                        .onAppear {
-                            animate()
-                        }
+                        .rotationEffect(.degrees(confetti.rotationInitial2D))
+                        .rotation3DEffect(.degrees(confetti.rotationIncremental3D), axis: confetti.rotationAxis, perspective: 0.5)
+//                        .onAppear {
+//                            animate(.linear(duration: 1).repeatCount(100, autoreverses: false))
+//                        }
                 }
                 .position(confetti.position)
                 
             }
         }
+        .onChange(of: counter) { _ in
+            animate(.linear(duration: 1))
+        }
         .onAppear {
-            // Add confetti every 0.1 seconds
-            Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
-                confetti.append(Confetti())
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                timer?.invalidate()
+                timer = nil
+            }
+            timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                counter += 1
+                if counter < maxConfetti {
+                    confetti.append(Confetti())
+                }
             }
         }
     }
     
-    func animate() {
-        withAnimation(.linear(duration: 1)) {
+    func animate(_ animation: Animation) {
+        withAnimation(animation) {
             for i in 0..<confetti.count {
                 let c = confetti[i]
-                confetti[i].position = CGPoint(x: c.position.x, y: c.position.y + c.duration)
-                confetti[i].rotationAnimation += 30
-            
+                confetti[i].position = CGPoint(x: c.position.x, y: c.position.y + c.yIncrementalDistance)
+                confetti[i].rotationIncremental3D += 30
             }
         }
     }
@@ -62,28 +73,34 @@ struct Confetti: Identifiable {
     let size: CGFloat
     
     var position: CGPoint
-    var rotationBase: Double
-    var rotationAnimation: Double = 0
-    let duration: TimeInterval
+    var rotationInitial2D: Double
+    var rotationIncremental3D: Double = 0
+    let yIncrementalDistance: Double
     
-    let axis: (x: CGFloat, y: CGFloat, z: CGFloat)
+    let rotationAxis: (x: CGFloat, y: CGFloat, z: CGFloat)
     
     init() {
-        self.color = Color(hue: Double.random(in: 0...1), saturation: Double.random(in: 0.5...1), brightness: Double.random(in: 0.5...1))
+        color = Color(
+            hue: Double.random(in: 0...1),
+            saturation: Double.random(in: 0.5...1),
+            brightness: Double.random(in: 0.5...1)
+        )
         
-        self.position = CGPoint(x: CGFloat.random(in: 0...UIScreen.main.bounds.width), y: -20)
-        self.duration = TimeInterval.random(in: 20...50) / 3
-        self.size = duration
-        self.rotationBase = Double.random(in: 5...40) / 3
+        position = CGPoint(
+            x: CGFloat.random(in: 0...UIScreen.main.bounds.width),
+            y: -20
+        )
+        yIncrementalDistance = Double.random(in: 15...45)
+        size = yIncrementalDistance / 3
+        rotationInitial2D = Double.random(in: 5...40) / 3
         
         switch Int.random(in: 0...2) {
         case 0:
-            self.axis = (x: 1, y: 0, z: 0.5)
+            rotationAxis = (x: 1, y: 0, z: 0.5)
         case 1:
-            self.axis = (x: 0.3, y: 1, z: 0.5)
+            rotationAxis = (x: 0.3, y: 1, z: 0.5)
         default:
-            self.axis = (x: 0, y: 0.5, z: 1)
+            rotationAxis = (x: 0, y: 0.5, z: 1)
         }
     }
 }
-
