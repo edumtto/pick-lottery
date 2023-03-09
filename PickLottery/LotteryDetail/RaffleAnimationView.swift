7d0ac2d6624a7eb3801b1bf5d1c3421c7da8beec
Eduardo Oliveira <edumottaoliveira@gmail.com>
2023-03-09 15:20:35 -0800

    Fix animation view

--- PickLottery/LotteryDetail/RaffleAnimationView.swift
+++ PickLottery/LotteryDetail/RaffleAnimationView.swift
@@ -1,127 +1,113 @@
 import SwiftUI
 import Foundation
 
-struct RaffleAnimationView: View {
-    struct RotatingItem: Identifiable {
-        let id: UUID = UUID()
-        let name: String
-        var offset: CGFloat
-        var rotationDegrees: Double
-        var scale: Double
-        //var opacity: CGFloat
+struct RaffleItem: Identifiable {
+    let id: UUID
+    let name: String
+    let color: Color
+    let offset: CGFloat
+    
+    init(_ entry: LotteryEntry, offset: CGFloat = 0) {
+        id = entry.id
+        name = entry.name
+        color = entry.color
+        self.offset = offset
     }
+}
+
+struct RaffleAnimationView: View {
+    let entries: [LotteryEntry]
+    let targetEntry: LotteryEntry
     
-//    let opacity = []
-    let rotation: [Double] =  [72, 48, 24, 0, -24, -48, -72]
-    let scale: [Double] =  [0.6, 0.7, 1, 1.2, 1, 0.7, 0.6]
-    let offset: [CGFloat] =  [-12, 4, -8, 0, 8, -4, -12]
+    @Binding var isRaffleFinished: Bool
     
-    let names: [String] = ["Ana", "Maurício", "Ricardo", "Edvania", "Tarcísio", "Marcelo", "Pedro"]
-    @State var nameIndex: Int = 0
-    
-    @State var items: [RotatingItem] =
-//        var list: [RotatingItem] = []
-//        for i in 0..<names.count {
-//            list.append(RotatingItem(name: names[i], offset: offset[i], rotationDegrees: rotation[i], scale: scale[i]))
-//        }
-//        return list
-//    }()
-//
-    [
-        .init(name: "Lana", offset: 12, rotationDegrees: 72, scale: 0.6),
-        .init(name: "Ana", offset: 4, rotationDegrees: 48, scale: 0.7),
-        .init(name: "João", offset: -8, rotationDegrees: 24, scale: 1),
-        .init(name: "Maria", offset: 0, rotationDegrees: 0, scale: 1.2),
-        .init(name: "Cristina", offset: 8, rotationDegrees: -24, scale: 1),
-        .init(name: "Júlia", offset: -4, rotationDegrees: -48, scale: 0.7),
-        .init(name: "Júlia", offset: -12, rotationDegrees: -72, scale: 0.6)
-    ]
-    
-    let limitOffset: Double = 100
-    let frameHeight: CGFloat = 52
+    @State private var nextEntryIndex: Int = 0
+    @State private var displayedItem: RaffleItem = .init(.init(""))
+    @State private var timer: Timer?
+    @State private var raffleFinishedAnimation = false
     
     var body: some View {
-        HStack {
-            Image(systemName: "arrow.right")
-                .scaleEffect(2)
+        ZStack {
+            Text(displayedItem.name)
+                .font(.headline)
                 .padding()
-            VStack {
-                ForEach(items) {
-                    Text($0.name)
-                        .padding()
-                        .frame(maxHeight: frameHeight)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(lineWidth: 1)
-                                .stroke(Color.accentColor)
-                        )
-                        //.offset(.init(width: 0, height: $0.offset))
-                        .rotation3DEffect(
-                            .degrees($0.rotationDegrees),
-                            axis: (x: 1, y: 0, z: 0),
-                            anchorZ: -30,
-                            perspective: 0.3
-                        )
-//                        .scaleEffect(x: $0.scale, y: $0.scale)
-                    
-                }
+                .frame(width: UIScreen.main.bounds.width - 32, height: 52)
 
-            }
-            .onTapGesture {
-                animate(times: 3, duration: 0.5)
-            }
+                .offset(.init(width: 0, height: displayedItem.offset))
+                .scaleEffect(raffleFinishedAnimation ? 1.4 : 1)
+            resultTextView
+
+        }
+        .onAppear {
+            animateRaffle(times: 50, interval: 0.02)
         }
     }
     
-    @State private var timer: Timer?
+    var resultTextView: some View {
+        VStack {
+            Text("Congratulations!")
+                
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(lineWidth: 1)
+                .stroke(displayedItem.color)
+            Spacer()
+        }.opacity(raffleFinishedAnimation ? 1 : 0)
+            .padding()
+    }
     
-    func animate(times: Int, duration: Double) {
-//        let animation = Animation.linear(duration: 0.1)
-//        withAnimation(animation) {
-////                items[i].offset = frameHeight
-//
-//
-//            items.insert(.init(name: nextName(), offset: 0, rotationDegrees: 20, scale: 0.8), at: 0)
-//            _ = items.popLast()
-//
-//
-//            for i in 0..<items.count {
-//                items[i].rotationDegrees = rotation[i]
-//                items[i].scale = scale[i]
-//                items[i].offset = offset[i]
-//            }
-//        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.timer?.invalidate()
+    func animateRaffle(times: Int, interval: Double) {
+        if times <= 1 {
+            endAnimation()
+            return
         }
         
-        timer = Timer.scheduledTimer(withTimeInterval: duration, repeats: true) { _ in
-            withAnimation(.linear(duration: duration)) {
-                    items.insert(.init(name: nextName(), offset: 0, rotationDegrees: 90, scale: 0.6), at: 0)
-                    _ = items.popLast()
-                    
-//                    for i in 0..<items.count {
-//                        items[i].rotationDegrees = rotation[i]
-//                        items[i].scale = scale[i]
-//                        items[i].offset = offset[i]
-//                    }
-                }
+        var counter = times
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
+            counter -= 1
+            moveOneItem()
+            
+            if counter == 0 {
+                animateRaffle(times: times - times / 2, interval: 1.2 * interval)
             }
+        }
     }
     
-    
-    func nextName() -> String {
-        let name = names[nameIndex]
-        nameIndex += 1
-        if nameIndex >= names.count {
-            nameIndex = 0
+    private func endAnimation() {
+        timer?.invalidate()
+        timer = nil
+        if displayedItem.id != targetEntry.id {
+            displayedItem = .init(targetEntry)
         }
-        return name
+        withAnimation {
+            raffleFinishedAnimation = true
+        }
+        isRaffleFinished = true
+    }
+    
+    private func moveOneItem() {
+        displayedItem = .init(nextEntry())
+    }
+    
+    private func nextEntry() -> LotteryEntry {
+        let entry = entries[nextEntryIndex]
+        nextEntryIndex += 1
+        if nextEntryIndex >= entries.count {
+            nextEntryIndex = 0
+        }
+        return entry
     }
 }
 
 struct RaffleAnimationView_Previews: PreviewProvider {
+    static let entriesMock: [LotteryEntry] = [
+        .init("João", weight: 1, winningCounter: 0),
+        .init("Maria", weight: 0, winningCounter: 1),
+        .init("James", weight: 1, winningCounter: 0),
+        .init("Ana", weight: 1.5, winningCounter: 2)
+    ]
+    
     static var previews: some View {
-        RaffleAnimationView()
+        RaffleAnimationView(entries: entriesMock, targetEntry: entriesMock[1], isRaffleFinished: .constant(false))
     }
 }
